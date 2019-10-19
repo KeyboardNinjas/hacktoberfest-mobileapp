b1bf346eee17fae70e5787d83fad99a8e56f1501
Jurie <jurie.spies@openvantage.co.za>
2019-10-19 17:54:15 +0200

    App.js edit for navigation

--- HackltoberFest_App/App.js
+++ HackltoberFest_App/App.js
@@ -1,88 +1,11 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-import Logo from './src/assets/images/LOGO.png'
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  Image,
-  StatusBar,
-  ImageBackground
-} from 'react-native';
+//Packages
+import React, {Component} from 'react';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+//Pages
+import Navigator from './Source/Router/Router';
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-     <View style={{backgroundColor: '#192249',height:'100%'}}>
-       <Image source={Logo} style={{ alignSelf: 'center',width:200,height:200}}  />
-       <Text style={{fontSize:20, alignSelf: 'center',}}>Welcome to hacktober</Text>
-     </View>
-    </>
-  );
-};
-
-const styles = StyleSheet.create({
-
-  headingText: {
-    fontSize:20,
-  },
-  backgroundColor: {
-    backgroundColor: '#192249',
-    height:'100%'
-  },
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
-
-export default App;
+export default class App extends Component {
+  render() {
+    return <Navigator />;
+  }
+}
