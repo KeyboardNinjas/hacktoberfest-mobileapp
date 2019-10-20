a1fda44b7405f7f127e911ddabc7d4a7bb493230
Jurie <jurie.spies@openvantage.co.za>
2019-10-20 16:59:32 +0200

    fixed Bottombar

--- src/components/BottomBar/BottomBar.component.js
+++ src/components/BottomBar/BottomBar.component.js
@@ -8,22 +8,27 @@ import { WHITE, YELLOW } from '../../data/consts';
 import { withNavigation } from 'react-navigation';
 
 const BottomBar = props => (
-  <View style={{ flexDirection: 'row' }}>
-    <TouchableOpacity
-      style={styles.catTab}
-      onPress={() => props.navigation.navigate('LandingPage')}>
-      <GitCat color={props.activeTab === 'catTab' ? YELLOW : WHITE} />
+  <>
+    <TouchableOpacity style={styles.plusTab} onPress={props.onAdd}>
+      <Add />
     </TouchableOpacity>
-    <View style={styles.plusTab}>
-      <TouchableOpacity onPress={props.onAdd}>
+    {/* <View style={styles.plusTab} onPress={props.onAdd}>
+      <TouchableOpacity>
         <Add />
       </TouchableOpacity>
+    </View> */}
+    <View style={{ flexDirection: 'row' }}>
+      <TouchableOpacity
+        style={styles.catTab}
+        onPress={() => props.navigation.navigate('LandingPage')}>
+        <GitCat color={props.activeTab === 'catTab' ? YELLOW : WHITE} />
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.catTab}
+        onPress={() => props.navigation.navigate('Friends')}>
+        <GitCats color={props.activeTab === 'catsTab' ? YELLOW : WHITE} />
+      </TouchableOpacity>
     </View>
-    <TouchableOpacity
-      style={styles.catTab}
-      onPress={() => props.navigation.navigate('Friends')}>
-      <GitCats color={props.activeTab === 'catsTab' ? YELLOW : WHITE} />
-    </TouchableOpacity>
-  </View>
+  </>
 );
 export default withNavigation(BottomBar);
